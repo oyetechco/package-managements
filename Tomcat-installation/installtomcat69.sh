a4081bdd86b09d93a86cf25b110150aac8ed5f47
oyetechco <115882289+oyetechco@users.noreply.github.com>
2023-05-31 11:08:42 -0400

    Update installtomcat69.sh

--- Tomcat-installation/installtomcat69.sh
+++ Tomcat-installation/installtomcat69.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
-#TOMCAT.SH
+# TOMCAT.SH
 # Steps for Installing tomcat9 on rhel7&8
 # install Java JDK 1.8+ as a pre-requisit for tomcat to run.
 # https://github.com/LandmakTechnology/package-management/tree/main/Tomcat-installation
+
+# install Java JDK 1.8+ as a pre-requisit for tomcat to run.
 sudo hostnamectl set-hostname tomcat
-cd /opt
-sudo yum install git wget vim -y
+cd /opt 
+sudo yum install git wget -y
 sudo yum install java-1.8.0-openjdk-devel -y
-# Download tomcat software and extract it.
-# dowanload and extract tomcat software
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.8/bin/apache-tomcat-10.1.8.tar.gz
-sudo tar -xvf apache-tomcat-10.1.8.tar.gz
-sudo rm apache-tomcat-10.1.8.tar.gz
-sudo mv apache-tomcat-10.1.8 tomcat10
-sudo chmod 777 -R /opt/tomcat10
-sudo sh /opt/tomcat10/bin/startup.sh
-# create a soft link to start and stop tomcat from anywhere
-# to manage tomcat as a service
-sudo ln -s /opt/tomcat10/bin/startup.sh /usr/bin/starttomcat
-sudo ln -s /opt/tomcat10/bin/shutdown.sh /usr/bin/stoptomcat
-sudo starttomcat
+# install wget unzip packages.
+sudo yum install wget unzip -y
+sudo wget  https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.75/bin/apache-tomcat-9.0.75.zip 
+sudo tar -xvf apache-tomcat-9.0.75.tar.gz
+sudo rm -rf apache-tomcat-9.0.75.tar.gz
+### rename tomcat for good naming convention
+sudo mv apache-tomcat-9.0.75 tomcat9
+### assign executable permissions to the tomcat home directory
+sudo chmod 777 -R /opt/tomcat9
+sudo chown ec2-user -R /opt/tomcat9
+### start tomcat
+sh /opt/tomcat9/bin/startup.sh
+# create a soft link to start and stop tomcat
+# This will enable us to manage tomcat as a service
+sudo ln -s /opt/tomcat9/bin/startup.sh /usr/bin/starttomcat
+sudo ln -s /opt/tomcat9/bin/shutdown.sh /usr/bin/stoptomcat
+starttomcat
 echo "end on tomcat installation"
-sudo su - ec2-user
